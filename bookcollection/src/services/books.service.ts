7de5d334305321cdae172af75937615ca718ce2d
ola marszalek <ola.0608@gmail.com>
2023-02-01 22:18:01 +0100

    books service added

---
+++ bookcollection/src/services/books.service.ts
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import { BookInterface } from '../Store/HelperInterface';
+
+const API = 'http://localhost:3004';
+
+const getBooks = () => {
+    const apiBooks = `${API}/books`
+    return axios.get(apiBooks)
+}
+
+const getOneBook = (id: string) => {
+    const apiOneBook = `${API}/books/${id}`
+    return axios.get(apiOneBook)
+}
+
+const getAuthors = () => {
+    const apiAuthors = `${API}/authors`
+    return axios.post(apiAuthors)
+}
+
+const addNewBook = (payload: BookInterface) => {
+    const apiBooks = `${API}/books`
+    return axios.post(apiBooks, payload) 
+}
+
+const addAuthor = (payload: any) => {
+const apiAuthors = `${API}/authors`
+return axios.post(apiAuthors, payload)
+}
+
+const deleteBook = (id: string) => {
+    const apiBooks = `${API}/books/${id}`
+    return axios.delete(apiBooks)
+}
+
+const editBook = (payload: BookInterface) => {
+    const apiBooks = `${API}/books/${payload.id}`
+    return axios.put(apiBooks, payload)
+}
+
+export {
+    getBooks,
+    getOneBook,
+    getAuthors,
+    addNewBook,
+    addAuthor,
+    deleteBook,
+    editBook
+}
