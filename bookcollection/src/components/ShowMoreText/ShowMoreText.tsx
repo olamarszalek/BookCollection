25fe1119da00a6487935b9f42a754c8add5b65cd
ola marszalek <ola.0608@gmail.com>
2023-02-01 22:27:35 +0100

    ShowMore component added

---
+++ bookcollection/src/components/ShowMoreText/ShowMoreText.tsx
@@ -0,0 +1,19 @@
+import { Button, Typography } from '@mui/material'
+import { FC, useState } from 'react'
+
+interface ShowText {
+    text: string
+}
+
+export const showMoreText: FC<ShowText> = ({text}) => {
+    const [showMore, setShowMore] = useState(false)
+
+    return (
+        <Typography variant="body2" color="text.secondary" sx={{margin: 1}}>
+            {showMore? text : `${text.substring(0, 100)}`}
+            {text.length > 100 && <Button variant="text" onClick={() => setShowMore(!showMore)}>
+                {showMore ? "Show less" : "Show more"}
+                </Button>}
+        </Typography>
+    )
+}
