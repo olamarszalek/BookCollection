8da0faaee617b36030942e8950052499a5d3ce70
ola marszalek <ola.0608@gmail.com>
2023-02-28 22:42:05 +0100

    Nav component style updated

--- bookcollection/src/components/Nav/Nav.tsx
+++ bookcollection/src/components/Nav/Nav.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import { FC } from 'react';
 import style from './Nav.module.scss'
 import { navElements } from '../../Store/HelperInterface';
+import { unstable_styleFunctionSx } from '@mui/system';
 
 interface PropsNav {
     children?: JSX.Element | JSX.Element[]
@@ -9,17 +10,18 @@ interface PropsNav {
 }
 
 export const Nav: FC<PropsNav> = props => {
+   
     return (
-        <nav>
+        <nav className={style.nav}>
             <ul>
                 <li>
-                    <Link to="/">Home</Link>
+                    <Link className={style.link} to="/">Home</Link>
                 </li>
                 <li>
-                    <Link to="/all">All Books</Link>
+                    <Link className={style.link} to="/all">All Books</Link>
                 </li>
                 <li>
-                    <Link to="/add">Add Books</Link>
+                    <Link className={style.link} to="/add">Add Books</Link>
                 </li>
             </ul>
         </nav>
