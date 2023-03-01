ed43e47c0f357e48148afb66c0a5313852fe6aaf
ola marszalek <ola.0608@gmail.com>
2023-03-01 23:29:49 +0100

    footer styling and icons

--- bookcollection/src/components/Footer/Footer.tsx
+++ bookcollection/src/components/Footer/Footer.tsx
@@ -1,10 +1,14 @@
 import { FC } from "react";
 import style from './Footer.module.scss'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+
+
 export const Footer: FC = () => {
     return (
         <footer>
             <div className={style.footer}>
-                <span> Developed by A. Marszałek |</span>
+                    <span> Developed by A. Marszałek | <FontAwesomeIcon icon={faGithub} className={style.icon} /> <FontAwesomeIcon icon={faLinkedin} className={style.icon}/></span>
             </div>
         </footer>
     )
